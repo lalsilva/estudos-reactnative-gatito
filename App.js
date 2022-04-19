4308524aa9435d7de6a8149db3ea7223776d7f0e
Luiz Silva <lasilva35@stefanini.com>
2022-04-19 17:34:44 -0300

    Exemplo de uso do Reactotron para debugar o aplicativo.

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import Reactotron from 'reactotron-react-native';
 
 import TelaPadrao from './src/componentes/TelaPadrao';
 import Rotas from './src/rotas';
 
+Reactotron.configure().useReactNative().connect();
+
 export default function App() {
+  Reactotron.log({
+    curso: 'Alura'
+  });
+
   return (
     <TelaPadrao>
       <Rotas />
