a0b3e6a87c5edcf105868076cea64024585e274c
Luiz Silva <lasilva35@stefanini.com>
2022-04-19 16:54:13 -0300

    Ajustada cores das partes de cima e de baixo do próprio aparelho para seguir o padrão do layout.

--- src/componentes/TelaPadrao/index.js
+++ src/componentes/TelaPadrao/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import { SafeAreaView, StatusBar } from 'react-native';
 
-import estilosGlobal from '../../estilos';
+import estilosGlobal, { cores } from '../../estilos';
+import estilos from './estilos';
 
 export default function TelaPadrao({ children }) {
     return (
-        <SafeAreaView style={estilosGlobal.preencher}>
-            <StatusBar />
-            {children}
-        </SafeAreaView>
+        <>
+            <SafeAreaView style={estilos.ajusteTela}>
+                <StatusBar backgroundColor={cores.roxo} />
+                {children}
+            </SafeAreaView>
+            <SafeAreaView style={estilos.ajusteTelaBaixo} />
+        </>
     )
 }
